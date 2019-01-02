7aa57f2d1037b7184cbc4c50c95e3137840b27dc
Daniel Palmer <daniel@0x0f.com>
2019-01-03 01:48:08 +0900

    Remove code that was split out into tlwpy

--- tlwpy/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='tlwpy',
-      version='0.1',
-      author='Daniel Palmer',
-      author_email='daniel@0x0f.com',
-      license='GPLv3',
-      packages=['tlwpy'],
-      zip_safe=False)
